887275254b6631b12f005a1f94d417b708529555
Alex Kadai <alexandrkadai@gmail.com>
2025-01-15 23:31:03 +0200

    Super2

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -4,9 +4,18 @@ type CardProps = {
   description: string;
 };
 
-export default function Card({src, title, description}:CardProps) {
-  return <div className="relative size-full rounded-md">
-    <video src={src} loop muted autoPlay className="absolute p-5  top-0 left-0 size-full object-cover object-center"></video>
-    {title}
-  </div>;
+export default function Card({ src, title, description }: CardProps) {
+  return (
+    <div className="relative size-full rounded-md">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center p-5"
+      ></video>
+      {title}
+      {description}
+    </div>
+  );
 }
