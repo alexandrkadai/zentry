5f058fb571c585ee54062197787947031aa2449f
Alex Kadai <alexandrkadai@gmail.com>
2025-01-17 21:31:15 +0200

    Story start

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,6 +1,20 @@
+import AnimatedTitle from "./ui/AnimatedTitle";
 
 export default function Story() {
   return (
-    <div>Story</div>
+    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+    <div className="flex size-full flex-col items-center py-10 pb-24">
+      <p className="font-general text-sm uppercase md:text-[16px] tracking-wider">
+        the multiversal ip world
+      </p>
+
+      <div className="relative size-full">
+        <AnimatedTitle
+          title="the story of a hidden realm"
+          className="mt-5 pointer-events-none mix-blend-difference relative z-10 text-balance"
+        />
+        </div>
+        </div>
+        </div>
   )
 }
